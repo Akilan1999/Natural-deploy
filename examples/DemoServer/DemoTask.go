16ad186f184a8f8a64e6a8b7313801e43a8b8ae1
Akilan <me@akilan.io>
2026-07-27 16:32:02 +0100

    small changes

--- examples/DemoServer/DemoTask.go
+++ examples/DemoServer/DemoTask.go
@@ -1,74 +1,83 @@
 package main
 
 import (
-	"fmt"
-	natural_deploy "github.com/Akilan1999/P2PRC-natural-deploy"
-	"github.com/Akilan1999/p2p-rendering-computation/client"
-	"github.com/Akilan1999/p2p-rendering-computation/p2p"
-	"os"
-	"time"
+    "fmt"
+    natural_deploy "github.com/Akilan1999/P2PRC-natural-deploy"
+    "github.com/Akilan1999/p2p-rendering-computation/client"
+    "github.com/Akilan1999/p2p-rendering-computation/p2p"
+    "os"
+    "time"
 )
 
 func main() {
-	_, err := os.Stat("Task.lock")
-	if err != nil {
-		natural_deploy.CreateTaskMachine("Test-2", nil)
-		os.Create("Task.lock")
-	}
+    _, err := os.Stat("Task.lock")
+    if err != nil {
+        // If you want to add a custom root node
+        //var rootnode natural_deploy.RootNode
+        //// make sure to fill in "<>" area with the appropriate information
+        //rootnode.IPAddress = "<IPV4>"
+        //rootnode.Port = "<ServerPort>"
+        //natural_deploy.CreateTaskMachine("Test-2", &rootnode)
 
-	// Create a 5 second delay from mapping port
-	time.Sleep(5 * time.Second)
+        // or
 
-	// ------------------- Create a task -----------------
-	var task natural_deploy.Task
-	task.Name = "Test"
-	task.TaskFile = "test.sh"
-	task.KillTaskFile = "kill.sh"
+        natural_deploy.CreateTaskMachine("Test-2", nil)
+        os.Create("Task.lock")
+    }
 
-	// Get all node information
-	table, err := p2p.ReadIpTable()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-		return
-	}
+    // Create a 5 second delay from mapping port
+    time.Sleep(5 * time.Second)
 
-	fmt.Println("IP table read")
+    // ------------------- Create a task -----------------
+    var task natural_deploy.Task
+    task.Name = "Test"
+    task.TaskFile = "test.sh"
+    task.KillTaskFile = "kill.sh"
 
-	// Searching for the information of a particular
-	// node information (In this example Test-2 node)
-	var machineInfo p2p.IpAddress
-	for i, _ := range table.IpAddress {
-		if table.IpAddress[i].Name == "Test-1" {
-			machineInfo = table.IpAddress[i]
-		}
-	}
+    // Get all node information
+    table, err := p2p.ReadIpTable()
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+        return
+    }
 
-	fmt.Println(machineInfo)
+    fmt.Println("IP table read")
 
-	// Allocate ports
-	var port client.ResponseMAPPort
-	port.PortNo = "8000"
-	task.ExposedPorts = append(task.ExposedPorts, &port)
+    // Searching for the information of a particular
+    // node information (In this example Test-2 node)
+    var machineInfo p2p.IpAddress
+    for i, _ := range table.IpAddress {
+        if table.IpAddress[i].Name == "Test-1" {
+            machineInfo = table.IpAddress[i]
+        }
+    }
 
-	task.NodeInfo = &machineInfo
-	// ---------------------------------------------------------
+    fmt.Println(machineInfo)
 
-	err = task.CreateTask()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-		return
-	}
+    // Allocate ports
+    var port client.ResponseMAPPort
+    port.PortNo = "8000"
+    task.ExposedPorts = append(task.ExposedPorts, &port)
 
-	// Create a 10 second delay from mapping port
-	time.Sleep(10 * time.Second)
+    task.NodeInfo = &machineInfo
+    // ---------------------------------------------------------
 
-	err = task.KillTask()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-		return
-	}
+    err = task.CreateTask()
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+        return
+    }
+
+    // Create a 10 second delay from mapping port
+    time.Sleep(10 * time.Second)
+
+    err = task.KillTask()
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+        return
+    }
 
 }
